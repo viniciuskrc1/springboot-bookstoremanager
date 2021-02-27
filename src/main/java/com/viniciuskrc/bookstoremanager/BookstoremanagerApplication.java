cb66caed50fdf2be9ede7a0a0543fe544c806a7b
Vinícius Kreusch da Cunha <viniciuskrc1@gmail.com>
2021-02-27 16:34:04 -0300

    Created .travis.yml file

--- src/main/java/com/viniciuskrc/bookstoremanager/BookstoremanagerApplication.java
+++ src/main/java/com/viniciuskrc/bookstoremanager/BookstoremanagerApplication.java
@@ -1,7 +1,9 @@
 package com.viniciuskrc.bookstoremanager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class BookstoremanagerApplication {
--- src/main/java/com/viniciuskrc/bookstoremanager/controller/BookController.java
+++ src/main/java/com/viniciuskrc/bookstoremanager/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.viniciuskrc.bookstoremanager.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -11,6 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
+    @ApiOperation(value = "Return an example hello world")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success method return")
+    })
     public String hello() {
         return "Hello Bookstorage Manager";
     }
